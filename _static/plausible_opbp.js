1e9eb5f5d7a0a0f325e67c919c72100e28558944
Firas Moosvi <firas@moosvi.com>
2022-08-18 12:02:49 -0700

    add plausible analytics

---
+++ _static/plausible_opbp.js
@@ -0,0 +1,9 @@
+var script = document.createElement("script");
+script.defer = true;
+script.src = "https://plausible.io/js/script.js";
+script.dataset.domain = "opbphysics";
+
+// optional if using proxy
+// script.dataset.api = 'https://yourproxy.com/api/event';
+
+document.getElementsByTagName("head")[0].appendChild(script);
